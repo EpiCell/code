067274de4fc3bbee5fc8230f78d7f5ca25bf25a8
Aaron Steele <eightysteele@gmail.com>
2011-11-12 19:35:53 -0800

    Modify to harvest India only.

--- cellmapper.py
+++ cellmapper.py
@@ -5,7 +5,7 @@ import urlparse
 def get_codes(line):
     """Return mnc, mcc, lac 3-tuple."""
     d = urlparse.parse_qs(line.split("window.open( 'map.php?")[1].split("'")[0])
-    return d['MNC'][0], d['MCC'][0], d['LAC'][0]
+    return int(d['MNC'][0]), int(d['MCC'][0]), d['LAC'][0]
     
 def get_base_numbers(mnc, mcc, lac):
     if lac.rfind(',') != -1:
@@ -34,23 +34,30 @@ def get_base_numbers(mnc, mcc, lac):
                 pass
     return signals
 
+
 if __name__ == '__main__':
+    process = False
     f = open('coordinates.csv', 'w')
     for line in open('data.html').read().split('\n'):
         val = ''
         if line.startswith('window.open('):
-            mnc, mcc, lac = get_codes(line)            
-            signals = get_base_numbers(mnc, mcc, lac)
-            if len(signals) == 0:
-                val = "-9999,-9999,"
-            else:
-                val = '%s,%s,' % (min(signals), max(signals))
-            f.write(val)
-            print val,
-        if line.startswith('position: new google.maps.LatLng('):
+            mnc, mcc, lac = get_codes(line)
+            if mcc in [404, 405]:
+                process = True
+                print 'Processing mcc %s' % mcc
+                signals = get_base_numbers(mnc, mcc, lac)
+                if len(signals) == 0:
+                    val = "-9999,-9999,"
+                else:
+                    val = '%s,%s,' % (min(signals), max(signals))
+                f.write(val)
+                print val,
+        if process and line.startswith('position: new google.maps.LatLng('):
+            process = False
             lat, lng = line.split('(')[1].split(')')[0].split(',')
             val += '%s,%s\n' % (lat.strip(),lng.strip())
             f.write(val)
+            f.flush()
             print val
     f.close()
         
